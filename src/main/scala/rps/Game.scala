89751ad4e53a015f557014dae09de63ba0eb7f4c
Miguel Pais <miguel.pais@abbott.com>
2017-10-19 11:52:38 +0200

    Implementing step3

--- src/main/scala/rps/Game.scala
+++ src/main/scala/rps/Game.scala
@@ -1,33 +1,55 @@
 package rps
 import scala.io.StdIn.readLine
 
+sealed abstract class Move {
+  def toString : String
+}
+case object ROCK extends Move {
+  override def toString = "Rock"
+}
+case object PAPER extends Move {
+  override def toString = "Paper"
+}
+case object SCISSORS extends Move {
+  override def toString = "Scissors"
+}
+case object INVALID extends Move {
+  override def toString = "Invalid Move!"
+}
+
 object Game {
   val randomGen = scala.util.Random
 
-  def moveRepr(move: Int): String = {
-    move match {
-      case 0 => "Rock"
-      case 1 => "Paper"
-      case 2 => "Scissors"
-      case _ => "Invalid move!"
-    }
+  implicit def intToMove(move: Option[Int]): Move = {
+   move match {
+    case Some(0) => ROCK
+    case Some(1) => PAPER
+    case Some(2) => SCISSORS
+    case _ => INVALID
   }
+}
 
-  def detectGameEndResult(playerMove: Int, computerMove: Int) : String = {
+  def detectGameEndResult(playerMove: Move, computerMove: Move) : String = {
     (playerMove, computerMove) match {
       case (x, y) if x == y => "Draw!"
-      case (0, 1) | (1, 2) | (2, 0) => "I Won!"
-      case (0, 2) | (1, 0) | (2, 1) => "You Won!"
+      case (ROCK, PAPER) | (PAPER, SCISSORS) | (PAPER, ROCK) => "I Won!"
+      case (ROCK, SCISSORS) | (PAPER, ROCK) | (SCISSORS, PAPER) => "You Won!"
       case (_, _) => "Invalid Round!!"
     }
   }
 
   def play() : Unit = {
-    val myPlay = randomGen.nextInt(3)
-    val userPlay = readLine("Choose your move between Rock(0), Paper(1) or Scissors(2): ").toInt
+    val myPlay : Move = Some(randomGen.nextInt(3))
 
-    println(s"You Played: ${moveRepr(userPlay)}")
-    println(s"I played: ${moveRepr(myPlay)}")
+    val userPlay : Move = try {
+      Some(readLine("Choose your move between Rock(0), Paper(1) or Scissors(2): ").toInt)
+    }
+    catch {
+      case ex: NumberFormatException => None
+    }
+
+    println(s"You Played: ${userPlay}")
+    println(s"I played: ${myPlay}")
     println(s"Result: ${detectGameEndResult(userPlay, myPlay)}")
   }
 }
